3738a6348c03a23a4b602b88622869baf0155d3a
VinhDev <congvinh3110@gmail.com>
2023-06-03 15:47:23 +0700

    api user

--- Controllers/User.Controller.js
+++ Controllers/User.Controller.js
@@ -1,5 +1,4 @@
 const UserModels = require("../Models/User.models");
-
 class UserController {
   sigUpUser(req, res, next) {
     const userName = req.query.userName;
@@ -15,17 +14,6 @@ class UserController {
       })
       .catch((err) => res.status(400).json("Tao tai khoan that bai"));
   }
-  login(req, res, next) {
-    const email = req.body.email;
-    const password = req.body.password;
-    UserModels.findOne({ email: email, password: password })
-      .then((data) => {
-        res.status(220).json("dang nhap thanh cong");
-      })
-      .catch((err) => {
-        res.status(500).json("Dang nhap that bai");
-      });
-  }
   updateUser(req, res, next) {
     const id = req.param.id;
     const newPassword = req.body.newPassword;
@@ -34,6 +22,18 @@ class UserController {
         res.json("Update thanh cong");
       })
       .catch((err) => res.status(500).json("Update That bai"));
+  }
+  api(req,res,next) {
+    UserModels.find({}).then((data) => {
+      if (!data) {
+        res.json("Khong co tai khoan");
+      }else{
+        res.json(data);
+      }
+      
+    }).catch((err) => {
+      res.status(500).json("Loi server")
+    })
   }
 }
 module.exports = new UserController();
--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 const userController = require("../Controllers/User.Controller");
 
 routes.post("/addUser", userController.sigUpUser);
-routes.post("/login", userController.login);
+routes.get("/api", userController.api);
 routes.put("/updateUser",userController.updateUser);
 
 module.exports = routes;
