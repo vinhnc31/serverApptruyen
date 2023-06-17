2a1b10457788d5836feae40f039874af5c89dbde
VinhDev <congvinh3110@gmail.com>
2023-06-18 05:51:37 +0700

    update 18/6

--- Controllers/User.Controller.js
+++ Controllers/User.Controller.js
@@ -16,17 +16,17 @@ class UserController {
       .catch((err) => res.status(400).json("Tao tai khoan that bai"));
   }
   async updateUser(req, res, next) {
-      try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        await buyer.findByIdAndUpdate(
-          { _id: req.params.id },
-          { password: hashedPassword }
-        );
-        res.send("Cập nhật mật khẩu thành công");
-      } catch (error) {
-        res.status(500).json("Update That bai")
-      }
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.confirmPassword, salt);
+      await UserModels.findByIdAndUpdate(
+        { _id: req.params.id },
+        { password: hashedPassword }
+      );
+      res.send("Cập nhật mật khẩu thành công");
+    } catch (error) {
+      res.status(500).json("Update That bai");
+    }
   }
   api(req, res, next) {
     UserModels.find({})
@@ -44,20 +44,27 @@ class UserController {
   async LoginUser(req, res, next) {
     try {
       const email = req.body.email;
+      const password = req.body.password;
       console.log(email);
-      let users = await UserModels.findOne({ email: email });
-      if (!users) {
-        res.send("email or password not found");
+      console.log(password);
+      if (!email || !password) {
+        res.status(400).json("Email and password are required");
         return;
-      } else {
-        users.comparePassword(req.body.password, function (err, isMatch) {
-          if (isMatch && !err) {
-            res.json(users);
-          }
-        });
       }
+      let user = await UserModels.findOne({ email: email });
+      if (!user) {
+        res.status(401).json("Email or password is incorrect");
+        return;
+      }
+      user.comparePassword(password, function (err, isMatch) {
+        if (isMatch && !err) {
+          res.json(user);
+        } else {
+          res.status(401).json("Email or password is incorrect");
+        }
+      });
     } catch (error) {
-      res.status(500).json("Loi roi");
+      res.status(500).json("Something went wrong");
     }
   }
   indexUser(req, res) {
